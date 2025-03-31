77e23fb552f7b04c1f96696bd7ed2eff5a3a903b
yahuaiii0923 <D00275358@student.dkit.ie>
2025-03-31 20:53:47 +0100

    created crawler class

---
+++ crawler.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Siew Yahuai on 31/03/2025.
+//
---
+++ crawler.h
@@ -0,0 +1,52 @@
+//
+// Created by Siew Yahuai on 31/03/2025.
+//
+
+#ifndef CRAWLER_H
+#define CRAWLER_H
+
+#include <iostream>
+#include <list>
+
+//coordinate on the 10x10 board
+struct Position{
+    int x;
+    int y;
+};
+
+enum class Direction{
+    North = 1,
+    East = 2,
+    South = 3,
+    West = 4
+  };
+
+class Crawler{
+private:
+    int id;
+    Position position;
+    Direction direction;
+    int size;
+    bool alive;
+    std::list<Position> path;
+
+
+public:
+    Crawler(int id, int x, int y, int dir, int size);
+
+    void setDirection(Direction dir);
+    void setAlive(bool status);
+    void increaseSize(int amount);
+
+    int getId() const;
+    Position getPosition() const;
+    Direction getDirection() const;
+    int getSize() const;
+    bool isAlive() const;
+    std::list<Position> getPath() const;
+
+    void move();
+    bool isWayBlocked() const;
+};
+
+#endif //CRAWLER_H
