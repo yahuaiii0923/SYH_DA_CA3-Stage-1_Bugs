f15299d39ea013885a427c306a3af99ed4f5a206
plo <dianaaboa@yahoo.com>
2025-04-02 21:09:58 +0100

    Implemented part 2 display all bugs functionality (2)

---
+++ Board.cpp
@@ -0,0 +1,5 @@
+//
+// Created by plo on 02/04/2025.
+//
+
+#include "Board.h"
---
+++ Board.h
@@ -0,0 +1,16 @@
+//
+// Created by plo on 02/04/2025.
+//
+
+#ifndef BOARD_H
+#define BOARD_H
+
+
+
+class Board {
+
+};
+
+
+
+#endif //BOARD_H
