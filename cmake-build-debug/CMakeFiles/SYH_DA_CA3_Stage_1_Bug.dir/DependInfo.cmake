ea54d8bce9658c243ccd69d5b42cd010ffd096b9
yahuaiii0923 <D00275358@student.dkit.ie>
2025-04-26 14:41:22 +0100

    fix minor issues

---
+++ cmake-build-debug/CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/yahuai/Documents/Y2/C++/Bugs_SYH_DA/Board.cpp" "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/Board.cpp.o" "gcc" "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/Board.cpp.o.d"
+  "/Users/yahuai/Documents/Y2/C++/Bugs_SYH_DA/Bug.cpp" "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/Bug.cpp.o" "gcc" "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/Bug.cpp.o.d"
+  "/Users/yahuai/Documents/Y2/C++/Bugs_SYH_DA/Crawler.cpp" "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/Crawler.cpp.o" "gcc" "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/Crawler.cpp.o.d"
+  "/Users/yahuai/Documents/Y2/C++/Bugs_SYH_DA/Hopper.cpp" "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/Hopper.cpp.o" "gcc" "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/Hopper.cpp.o.d"
+  "/Users/yahuai/Documents/Y2/C++/Bugs_SYH_DA/main.cpp" "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/main.cpp.o" "gcc" "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/Board.cpp.o"
+  "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/Board.cpp.o.d"
+  "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/Bug.cpp.o"
+  "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/Bug.cpp.o.d"
+  "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/Crawler.cpp.o"
+  "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/Crawler.cpp.o.d"
+  "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/Hopper.cpp.o"
+  "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/Hopper.cpp.o.d"
+  "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/main.cpp.o"
+  "CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/main.cpp.o.d"
+  "SYH_DA_CA3_Stage_1_Bug"
+  "SYH_DA_CA3_Stage_1_Bug.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SYH_DA_CA3_Stage_1_Bug.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
