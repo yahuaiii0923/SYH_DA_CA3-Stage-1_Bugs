99147298afe8a976b6d03cd01dae894e89c75740
plo <dianaaboa@yahoo.com>
2025-04-27 22:32:09 +0100

    Finally completed feature 10 which implements new bug and the bug I implemented was a zigzagger bug which first moves forward then moves diagonal to replicate a zigzag motion. Tested and ensured new bug was implemented correctly

---
+++ Zigzagger.cpp
@@ -0,0 +1,5 @@
+//
+// Created by plo on 27/04/2025.
+//
+
+#include "Zigzagger.h"
---
+++ Zigzagger.h
@@ -0,0 +1,16 @@
+//
+// Created by plo on 27/04/2025.
+//
+
+#ifndef ZIGZAGGER_H
+#define ZIGZAGGER_H
+
+
+
+class Zigzagger {
+
+};
+
+
+
+#endif //ZIGZAGGER_H
